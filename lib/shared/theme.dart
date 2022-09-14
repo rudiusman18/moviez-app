6aeab175ba2ac8da569d346201680d0fdaff1360
rudi-usman <commander.heaven@gmail.com>
2022-09-14 09:39:16 +0700

    Latest Update Rudi Usman

---
+++ lib/shared/theme.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+// color template
+const Color primaryColor = Color(0xff0D0846);
+const Color subtitleColor = Color(0xff929292);
+const Color whiteColor = Color(0xffFFFFFF);
+const Color backgroundColor1 = Color(0xffFDFDFE);
+const Color backgroundColor2 = Color(0xffF3F4F8);
+
+// TextStyle template
+const TextStyle avenirBlackTextStyle = TextStyle(fontFamily: 'Avenir-Black');
+const TextStyle avenirBookTextStyle = TextStyle(fontFamily: 'Avenir-Book');
+const TextStyle avenirHeavyTextStyle = TextStyle(fontFamily: 'Avenir-Heavy');
+const TextStyle avenirMediumTextStyle = TextStyle(fontFamily: 'Avenir-Medium');
+const TextStyle avenirRomanTextStyle = TextStyle(fontFamily: 'Avenir-Roman');
